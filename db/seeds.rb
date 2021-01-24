df5dff8ec8a90296a15c1810508fb756a73397d7
Walter <jingzheng1989@gmail.com>
2021-01-24 23:12:45 +1100

    Add likes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,19 @@
 
 user1 = User.create(name: 'Alice', email: 'test1@test.com', password: '12345678')
 user2 = User.create(name: 'Bob', email: 'test2@test.com', password: '12345678')
+user3 = User.create(name: 'C', email: 'test2@test.com', password: '12345678')
 
-Post.create(user: user1, content: 'Great weather today!')
-Post.create(user: user1, content: 'Beautiful Beach!')
-Post.create(user: user1, content: 'Sydney is hot.')
+post1 = Post.create(user: user1, content: 'Great weather today!')
+post2 = Post.create(user: user1, content: 'Beautiful Beach!')
+post3 = Post.create(user: user1, content: 'Sydney is hot.')
 Post.create(user: user1, content: 'Yay Rails!')
 Post.create(user: user2, content: 'PHP is the best!')
 Post.create(user: user2, content: 'Let\'s go drinking')
 Post.create(user: user2, content: 'Scaffold is fun!')
+
+Like.create(user: user2, post: post1)
+Like.create(user: user2, post: post2)
+Like.create(user: user2, post: post3)
+Like.create(user: user3, post: post1)
+Like.create(user: user3, post: post2)
+Like.create(user: user3, post: post3)
